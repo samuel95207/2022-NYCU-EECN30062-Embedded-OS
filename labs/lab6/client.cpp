ab820e9ac8b52c058041510f44f336464974abe9
Samuel Huang <lspss95207@gmail.com>
2022-12-09 12:40:20 +0800

    lab6: Update client to create multiple connection

--- labs/lab6/client.cpp
+++ labs/lab6/client.cpp
@@ -22,30 +22,29 @@ int main(int argc, char** argv) {
     int loopCount = atoi(argv[5]);
 
 
-    int connfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in client_sin;
-    memset(&client_sin, 0, sizeof(client_sin));
-    client_sin.sin_family = AF_INET;
-    client_sin.sin_addr.s_addr = inet_addr(ip);
-    client_sin.sin_port = htons(port);
 
     // connect
-    if (connect(connfd, (struct sockaddr*)&client_sin, sizeof(client_sin)) == -1) {
-        printf("Error connect to server\n");
-        exit(-1);
-    }
-
-
     for (int i = 0; i < loopCount; i++) {
+        int connfd = socket(AF_INET, SOCK_STREAM, 0);
+
+        struct sockaddr_in client_sin;
+        memset(&client_sin, 0, sizeof(client_sin));
+        client_sin.sin_family = AF_INET;
+        client_sin.sin_addr.s_addr = inet_addr(ip);
+        client_sin.sin_port = htons(port);
+        while (connect(connfd, (struct sockaddr*)&client_sin, sizeof(client_sin)) == -1) {
+            printf("Error connect to server\n");
+        }
         ostringstream oss;
         oss << command << " " << ammount << "\n";
         string output = oss.str();
         write(connfd, output.c_str(), output.length());
+        output = "exit\n";
+        write(connfd, output.c_str(), output.length());
+        close(connfd);
     }
 
-    string output = "exit\n";
-    write(connfd, output.c_str(), output.length());
-    close(connfd);
+
     return 0;
 }
